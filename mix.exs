2fe445ce44d174c8c5510904d20d0a86962dd167
rodrigocitadin <citadinrodrigo@gmail.com>
2024-11-26 22:01:03 -0300

    add ex_machina and faker

--- mix.exs
+++ mix.exs
@@ -42,7 +42,9 @@ defmodule Bbank.MixProject do
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.1.1"},
       {:plug_cowboy, "~> 2.7"},
-      {:argon2_elixir, "~> 4.0"}
+      {:argon2_elixir, "~> 4.0"},
+      {:ex_machina, "~> 2.8.0", only: :test},
+      {:faker, "~> 0.18", only: :test}
     ]
   end
 
