23eeb8a4aa3805a9b7ec721a31cb45d666adfab6
rodrigocitadin <citadinrodrigo@gmail.com>
2024-12-21 00:41:08 -0300

    fix account changeset behavior

--- lib/bbank/accounts/account.ex
+++ lib/bbank/accounts/account.ex
@@ -5,6 +5,7 @@ defmodule Bbank.Accounts.Account do
   alias Bbank.Users.User
 
   @account_fields [:user_id]
+  @required_fields [:balance | @account_fields]
 
   @derive {Jason.Encoder, only: [:id, :user_id, :balance]}
   schema "accounts" do
@@ -16,8 +17,18 @@ defmodule Bbank.Accounts.Account do
 
   def changeset(params) do
     %__MODULE__{}
-    |> cast(params, @account_fields)
-    |> validate_required(@account_fields)
+    |> do_validations(params, @account_fields)
+  end
+
+  def changeset(account, params) do
+    account
+    |> do_validations(params, @required_fields)
+  end
+
+  defp do_validations(changeset, params, cast_params, required_params \\ @required_fields) do
+    changeset
+    |> cast(params, cast_params)
+    |> validate_required(required_params)
     |> unique_constraint(:user_id)
     |> check_constraint(:balance, name: :balance_must_be_positive)
   end
